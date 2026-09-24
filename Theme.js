e67ff8d70b7bf718020091abf94729cd673da2af
Chaliptul <apinutchaya.w@ku.th>
2026-09-24 15:43:06 +0700

    Refactor Theme.js by removing topInset
    Removed topInset constant and adjusted color definitions.

---
+++ Theme.js
@@ -0,0 +1,12 @@
+import { Platform, StatusBar } from 'react-native';
+
+export const colors = {
+  bg: '#FBF9F5',         
+  card: '#FFFFFF',       
+  border: '#E6DFD5',   
+  text: '#3D3731',       
+  dim: '#8C827A',      
+  cyan: '#A0826C',     
+  green: '#8A9A86',       
+  red: '#c26042',        
+};
